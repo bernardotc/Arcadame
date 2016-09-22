3af26461191cd7f86eca4fd2cce178e6ffc58904
Bernardo Trevino <bernardotc94@gmail.com>
2016-09-22 18:41:40 -0500

    Started using semantic analysis. Already implemented directory of functions and table for variables.

--- arcadame.py
+++ arcadame.py
@@ -6,13 +6,35 @@
 # A simple scanner and parser for the languange Arcadame
 # -----------------------------------------------------------------------------
 
+
+# int = 101
+# float = 102
+# string = 103
+# char = 104
+# boolean = 105
+# Immovable = 201
+# MovingAvatar = 202
+# ShootingAvatar = 203
+# Missile = 204
+# Spawner = 205
+# Passive = 206
+
 import sys
 sys.path.insert(0, "../..")
 
 if sys.version_info[0] >= 3:
     raw_input = input
 
-debug = True
+debug = False
+
+listSprites = []
+killSprites = []
+gameAttrs = {}
+gameSections = {}
+functionDirectory = {}
+variables = {}
+globalVariables = {}
+typeOfVariable = ""
 
 class LexerError(Exception):
     def __init__(self, value):
@@ -31,6 +53,30 @@ class SemanticError(Exception):
         self.value = value
     def __str__(self):
         return repr(self.value)
+
+def convertAtomicTypeToCode(type):
+    if (type == "int"):
+        return 101
+    elif (type == "float"):
+        return 102
+    elif (type == "string"):
+        return 103
+    elif (type == "char"):
+        return 104
+    elif (type == "boolean"):
+        return 105
+    elif (type == "Immovable"):
+        return 201
+    elif (type == "MovingAvatar"):
+        return 202
+    elif (type == "ShootingAvatar"):
+        return 203
+    elif (type == "Missile"):
+        return 204
+    elif (type == "Spawner"):
+        return 205
+    elif (type == "Passive"):
+        return 206
 
 # Define token names
 tokens = (
@@ -213,7 +259,7 @@ def t_SCORECHANGE(t):
     return t
 
 def t_TRANSFORMTO(t):
-    'bounceForward'
+    'transfromTo'
     if (debug):
         print(t)
     return t
@@ -225,7 +271,7 @@ def t_BOUNCEFORWARD(t):
     return t
 
 def t_CHANGEHEALTHPOINTS(t):
-    'Program'
+    'changeHealthPoints'
     if (debug):
         print(t)
     return t
@@ -597,9 +643,17 @@ start = 'programa'
 def p_programa(p):
     '''programa : SIMPLEGAME ID D_2P sprites interactions goals mapping map block BIG_END
                 | PROGRAM ID D_2P block BIG_END'''
+    global functionDirectory, gameSections
+    # Save SimpleGame in function directory
+    if (p[1] == "SimpleGame"):
+        functionDirectory["SimpleGame"] = {"sections" : gameSections}
+        gameSections = {}
 
 def p_sprites(p):
     '''sprites : SPRITESET D_2P sprite D_PYC more_sprites SMALL_END'''
+    global gameSections, variables
+    gameSections[p[1]] = variables
+    variables = {}
 
 def p_more_sprites(p):
     '''more_sprites : sprite D_PYC more_sprites
@@ -607,6 +661,11 @@ def p_more_sprites(p):
 
 def p_sprite(p):
     '''sprite : ID EQUAL sprite_type sprite_attrs'''
+    global variables, gameAttrs, type
+    if (variables.has_key(p[1])):
+        raise SemanticError("Repeated identifier for sprite: " + p[1])
+    variables[p[1]] = {"type": convertAtomicTypeToCode(type), "attrs": gameAttrs}
+    gameAttrs = {}
 
 def p_sprite_type(p):
     '''sprite_type : IMMOVABLE
@@ -615,6 +674,8 @@ def p_sprite_type(p):
                    | MISSILE
                    | SPAWNER
                    | PASSIVE'''
+    global type
+    type = p[1]
 
 def p_sprite_attrs(p):
     '''sprite_attrs : sprite_attr more_sprite_attrs
@@ -638,9 +699,17 @@ def p_sprite_attr(p):
                    | LIMITHEALTHPOINTS EQUAL INT_CT
                    | WEAPONSPRITE EQUAL ID
                    | ORIENTATION EQUAL MOVEMENT_CT'''
+    global variables, gameAttrs
+    if (p[1] == "generatedSprite" or p[1] == "weaponSprite"):
+        if (not variables.has_key(p[3])):
+            raise SemanticError("Use of undeclared sprite id: " + p[3])
+    gameAttrs[p[1]] = p[3]
 
 def p_interactions(p):
     '''interactions : INTERACTIONLIST D_2P interaction D_PYC more_interactions SMALL_END'''
+    global gameSections, variables
+    gameSections[p[1]] = variables
+    variables = {}
 
 def p_more_interactions(p):
     '''more_interactions : interaction D_PYC more_interactions
@@ -648,10 +717,29 @@ def p_more_interactions(p):
 
 def p_interaction(p):
     '''interaction : ID more_spritesid ENCOUNTERS ID EQUAL action more_actions'''
+    global gameSections, listSprites, gameAttrs, variables
+    sprites = gameSections["SpriteSet"]
+    if (not sprites.has_key(p[1])):
+        raise SemanticError("Use of undeclared sprite id: " + p[1])
+    if (not sprites.has_key(p[4])):
+        raise SemanticError("Use of undeclared sprite id: " + p[4])
+    listSprites.append(p[1])
+    listSprites.append(p[4])
+    variables[tuple(listSprites)] = {"actions": gameAttrs}
+    listSprites = []
+    gameAttrs = {}
 
 def p_more_spritesid(p):
     '''more_spritesid : D_C ID more_spritesid
                       | '''
+    global gameSections, listSprites
+    sprites = gameSections["SpriteSet"]
+    try:
+        if (not sprites.has_key(p[2])):
+            raise SemanticError("Use of undeclared sprite id: " + p[2])
+        listSprites.append(p[2])
+    except IndexError:
+        return
 
 def p_more_actions(p):
     '''more_actions : D_C action more_actions
@@ -667,13 +755,48 @@ def p_action(p):
               | ADDTIMER TIME EQUAL INT_CT TRANSFORMALL D_PA ID D_C ID D_PC
               | SHIELDFROM D_PA KILLSPRITE D_C ID D_PC
               | PULLWITHIT'''
+    global gameSections, listSprites, gameAttrs, killSprites
+    sprites = gameSections["SpriteSet"]
+    if (p[1] == "killSprite"):
+        gameAttrs[p[1]] = killSprites
+        killSprites = []
+    elif (p[1] == "stepBack" or p[1] == "pullWithIt" or p[1] == "bounceForward"):
+        gameAttrs[p[1]] = {}
+    elif (p[1] == "transformTo"):
+        if (not sprites.has_key(p[3])):
+            raise SemanticError("Use of undeclared sprite id: " + p[3])
+        gameAttrs[p[1]] = p[3]
+    elif (p[1] == "addTimer"):
+        if (not sprites.has_key(p[7])):
+            raise SemanticError("Use of undeclared sprite id: " + p[7])
+        if (not sprites.has_key(p[9])):
+            raise SemanticError("Use of undeclared sprite id: " + p[9])
+        gameAttrs[p[1]] = {p[2]: p[4], "from": p[7], "to": p[9]}
+    elif (p[1] == "shieldFrom"):
+        if (not sprites.has_key(p[5])):
+            raise SemanticError("Use of undeclared sprite id: " + p[5])
+        gameAttrs[p[1]] = p[5]
+    else:
+        print(p[3])
+        gameAttrs[p[1]] = p[3]
 
 def p_kill_id(p):
     '''kill_id : ID
                | '''
+    global gameSections, killSprites
+    sprites = gameSections["SpriteSet"]
+    try:
+        if (not sprites.has_key(p[1])):
+            raise SemanticError("Use of undeclared sprite id: " + p[1])
+        killSprites.append(p[1])
+    except IndexError:
+        return
 
 def p_goals(p):
     '''goals : TERMINATIONGOALS D_2P goal D_PYC more_goals SMALL_END'''
+    global gameSections, variables
+    gameSections[p[1]] = variables
+    variables = {}
 
 def p_more_goals(p):
     '''more_goals : goal D_PYC
@@ -681,14 +804,32 @@ def p_more_goals(p):
 
 def p_goal(p):
     '''goal : goal_type LIMIT EQUAL INT_CT WIN EQUAL BOOLEAN_CT'''
+    global variables, gameAttrs, type, listSprites
+    gameAttrs = {p[2]: p[4], p[5]: p[7]}
+    variables[tuple(listSprites)] = {"type": type, "attrs": gameAttrs}
+    listSprites = []
+    gameAttrs = {}
 
 def p_goal_type(p):
     '''goal_type : SPRITECOUNTER SPRITE EQUAL ID
                  | MULTISPRITECOUNTER sprite_list
                  | TIMEOUT'''
+    global gameSections, listSprites, type
+    sprites = gameSections["SpriteSet"]
+    type = p[1]
+    if (type == "spriteCounter"):
+        if (not sprites.has_key(p[4])):
+            raise SemanticError("Use of undeclared sprite id: " + p[4])
+        listSprites.append(p[4])
 
 def p_sprite_list(p):
     '''sprite_list : SPRITE EQUAL ID more_sprite_list'''
+    global gameSections, listSprites
+    sprites = gameSections["SpriteSet"]
+    if (not sprites.has_key(p[3])):
+        raise SemanticError("Use of undeclared sprite id: " + p[3])
+    listSprites.append(p[2])
+    return
 
 def p_more_sprite_list(p):
     '''more_sprite_list : D_C sprite_list
@@ -696,6 +837,9 @@ def p_more_sprite_list(p):
 
 def p_mapping(p):
     '''mapping : MAPPING D_2P mapping_rule D_PYC more_mapping_rules SMALL_END'''
+    global gameSections, variables
+    gameSections[p[1]] = variables
+    variables = {}
 
 def p_more_mapping_rules(p):
     '''more_mapping_rules : mapping_rule D_PYC more_mapping_rules
@@ -703,16 +847,46 @@ def p_more_mapping_rules(p):
 
 def p_mapping_rule(p):
     '''mapping_rule : CHAR_CT EQUAL ID more_spritesid'''
+    global gameSections, listSprites, variables
+    sprites = gameSections["SpriteSet"]
+    if (not sprites.has_key(p[3])):
+        raise SemanticError("Use of undeclared sprite id: " + p[3])
+    if (p[1] in variables.values()):
+        raise SemanticError("Repeated char for mapping: " + p[1])
+    listSprites = [p[3]] + listSprites
+    variables[p[1]] = listSprites
+    listSprites = []
 
 def p_map(p):
     '''map : MAP D_2P STRING_CT D_PYC more_tiles SMALL_END'''
+    global gameSections, listSprites
+    mapping = gameSections["Mapping"]
+    for mapping_id in p[3]:
+        if ((not mapping.has_key("'" + mapping_id + "'")) and mapping_id != '"'):
+            raise SemanticError("Use of undeclared char for mapping: " + mapping_id)
+    listSprites = [p[3]] + listSprites
+    gameSections[p[1]] = listSprites
+    listSprites = []
 
 def p_more_tiles(p):
     '''more_tiles : STRING_CT D_PYC more_tiles
                   | '''
+    global listSprites, gameSections
+    mapping = gameSections["Mapping"]
+    try:
+        for mapping_id in p[1]:
+            if ((not mapping.has_key("'" + mapping_id + "'")) and mapping_id != '"'):
+                raise SemanticError("Use of undeclared char for mapping: " + mapping_id)
+        listSprites = [p[1]] + listSprites
+    except IndexError:
+        return
 
 def p_block(p):
     '''block : vars functions main_block'''
+    # Save global variables with its attributes.
+    global globalVariables, variables
+    globalVariables = variables
+    variables = {}
 
 def p_vars(p):
     '''vars : var D_PYC vars
@@ -721,6 +895,16 @@ def p_vars(p):
 def p_var(p):
     '''var : VAR type D_2P ID
            | VAR type D_CA INT_CT D_CC D_2P ID'''
+    global variables, type
+    # Get var attributes and save it.
+    if (p[3] != ':'):
+        if (variables.has_key(p[7])):
+            raise SemanticError("Repeated identifier for variable: " + p[7])
+        variables[p[7]] = {"type": convertAtomicTypeToCode(type)+1000}
+    else:
+        if (variables.has_key(p[4])):
+            raise SemanticError("Repeated identifier for variable: " + p[4])
+        variables[p[4]] = {"type": convertAtomicTypeToCode(type)}
 
 def p_type(p):
     '''type : INT
@@ -728,6 +912,8 @@ def p_type(p):
             | STRING
             | CHAR
             | BOOLEAN'''
+    global type
+    type = p[1]
 
 def p_functions(p):
     '''functions : function functions
@@ -735,6 +921,12 @@ def p_functions(p):
 
 def p_function(p):
     '''function : FUNC ID D_PA parameters D_PC EQUAL type D_2P code_block SMALL_END'''
+    global functionDirectory, variables
+    # Save the function with its variables
+    if (functionDirectory.has_key(p[2])):
+        raise SemanticError("Repeated identifier for function: " + p[2])
+    functionDirectory[p[2]] = {"variables": variables}
+    variables = {}
 
 def p_parameters(p):
     '''parameters : parameter more_parameters
@@ -746,6 +938,16 @@ def p_parameter(p):
 def p_type_parameter(p):
     '''type_parameter : D_AMP ID
                       | ID'''
+    global variables, type
+    # Get parameter attributes and save it.
+    if (p[1] == '&'):
+        if (variables.has_key(p[2])):
+            raise SemanticError("Repeated identifier for variable: " + p[2])
+        variables[p[2]] = {"type": convertAtomicTypeToCode(type), "reference_parameter": True}
+    else:
+        if (variables.has_key(p[1])):
+            raise SemanticError("Repeated identifier for variable: " + p[1])
+        variables[p[1]] = {"type": convertAtomicTypeToCode(type), "reference_parameter": False}
 
 def p_more_parameters(p):
     '''more_parameters : D_C parameter more_parameters
@@ -753,6 +955,12 @@ def p_more_parameters(p):
 
 def p_main_block(p):
     '''main_block : MAIN D_2P code_block SMALL_END'''
+    global functionDirectory, variables
+    # Save Main function with its attributes.
+    if (functionDirectory.has_key(p[1])):
+        raise SemanticError("Repeated identifier for function: " + p[1])
+    functionDirectory[p[1]] = {"variables": variables}
+    variables = {}
 
 def p_code_block(p):
     '''code_block : vars mini_block'''
@@ -882,13 +1090,19 @@ def p_error(p):
 import ply.yacc as yacc
 parser = yacc.yacc()
 
-
-# Main. It expects
+# Main.
 while 1:
     try:
         s = raw_input('arcadame > ')
     except EOFError:
         break
+    functionDirectory = {"global": {}}
+    variables = {}
+    globalVariables = {}
+    gameSections = {}
+    gameAttrs = {}
+    killSprites = []
+    listSprites = []
     with open(s) as fp:
         completeString = ""
         for line in fp:
@@ -896,5 +1110,6 @@ while 1:
         # print completeString
         try:
             parser.parse(completeString)
+            print functionDirectory
         except EOFError:
             break
